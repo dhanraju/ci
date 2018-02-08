343e007d1e23f17acf3537f6eb27df9c0a2b60bb
dhanraju <dhanunjay.raju@gmail.com>
2018-02-07 22:13:22 -0800

    Commented source and pip commands.

--- run_pylint.sh
+++ run_pylint.sh
@@ -2,10 +2,10 @@ if [ ! -d venv ] ; then
 
    virtualenv --python=python2.7 venv
 fi
-source venv/bin/activate
+# source venv/bin/activate
 export PYTHONPATH="$PWD:$PYTHONPATH"
 
-pip install pylint
+# pip install pylint
 
 ## cd repo
 ### Need this because some strange control sequences when using default TERM=xterm
