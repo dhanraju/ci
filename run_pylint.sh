e244a614f760c6b8cce6bf3f83881b89566ae5db
dhanraju <dhanunjay.raju@gmail.com>
2018-02-07 22:02:09 -0800

    Fix run_pylint.sh script.

--- run_pylint.sh
+++ run_pylint.sh
@@ -7,10 +7,14 @@ export PYTHONPATH="$PWD:$PYTHONPATH"
 
 pip install pylint
 
-cd repo
+## cd repo
 ### Need this because some strange control sequences when using default TERM=xterm
 export TERM="linux"
 
-## || exit 0 because pylint only exits with 0 if everything is correct
-pylint --rcfile=pylint.cfg $(find . -maxdepth 1 -name "*.py" -print) MYMODULE/ > pylint.log || exit 0
+## || exit 0 because pylint only exits with 0 if everything is correct. maxdepth
+## is 2; because we need search *.py files under main directory.
+pylint --rcfile=pylint.cfg $(find . -maxdepth 2 -name "*.py" -print) main/ > pylint.log || exit 0
+
+## If we have more subfolders, we could just use below statement.
+## pylint --rcfile=pylint.cfg $(find main/ -name "*.py" -print) main/ > pylint.log || exit 0
 
